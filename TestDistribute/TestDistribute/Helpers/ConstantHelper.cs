166a12bd023fb0d7466c06ca0020d979bdfd55af
ahouhel <aliaksandr.Houhel@cogniteq.com>
2019-03-07 13:21:32 +0300

    added app center token

--- TestDistribute/TestDistribute/Helpers/ConstantHelper.cs
+++ TestDistribute/TestDistribute/Helpers/ConstantHelper.cs
@@ -2,6 +2,6 @@
 {
     public static class ConstantHelper
     {
-        public static string AppCenterToken = "Need to change";
+        public static string AppCenterToken = "8973601c-e3f5-4e18-809a-a9073eb35c81";
     }
 }
